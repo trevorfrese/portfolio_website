a04a3e63260dd9a2dcb013940365c1a10c795e99
Trevor Frese <trevor.frese@gmail.com>
2018-02-09 19:32:31 -0800

    Turn server on

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
 
-var x = 0;
+const app = express();
+app.listen(3000, () => console.log('App listening on port 3000!'));
+app.use(express.static(path.join(__dirname, '/public')));
